907663d2613eb0edde5a570072bfd8b55507a8ac
sachithra-manamperi <manamper-se18028@stu.kln.ac.lk>
2022-02-09 21:39:38 +0530

    Third Commit

--- ELEKSTORE/src/main/java/Interfaces/BrandAdder.java
+++ ELEKSTORE/src/main/java/Interfaces/BrandAdder.java
@@ -26,40 +26,45 @@ public class BrandAdder extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnAddProduct = new java.awt.Button();
-        btnAddProduct1 = new java.awt.Button();
-        btnAddProduct2 = new java.awt.Button();
+        btnViewProducts = new java.awt.Button();
+        btnViewBrands = new java.awt.Button();
+        btnAddCategory = new java.awt.Button();
         btnAddProduct3 = new java.awt.Button();
-        btnAddProduct4 = new java.awt.Button();
+        btnAddBrand = new java.awt.Button();
         btnAddProduct5 = new java.awt.Button();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        btnViewCustomers = new java.awt.Button();
+        btnDailySales = new java.awt.Button();
+        btnTodaySales = new java.awt.Button();
+        btnLogOut = new java.awt.Button();
+        btnViewOrders = new java.awt.Button();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btnAddProduct.setBackground(new java.awt.Color(153, 153, 255));
-        btnAddProduct.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
-        btnAddProduct.setLabel("Add Product");
+        btnViewProducts.setBackground(new java.awt.Color(153, 153, 255));
+        btnViewProducts.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnViewProducts.setLabel("View Products");
 
-        btnAddProduct1.setBackground(new java.awt.Color(153, 153, 255));
-        btnAddProduct1.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
-        btnAddProduct1.setLabel("Add Product");
+        btnViewBrands.setBackground(new java.awt.Color(153, 153, 255));
+        btnViewBrands.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnViewBrands.setLabel("View Brands");
 
-        btnAddProduct2.setBackground(new java.awt.Color(153, 153, 255));
-        btnAddProduct2.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
-        btnAddProduct2.setLabel("Add Product");
+        btnAddCategory.setBackground(new java.awt.Color(153, 153, 255));
+        btnAddCategory.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnAddCategory.setLabel("Add Category");
 
         btnAddProduct3.setBackground(new java.awt.Color(153, 153, 255));
         btnAddProduct3.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
         btnAddProduct3.setLabel("Add Product");
 
-        btnAddProduct4.setBackground(new java.awt.Color(153, 153, 255));
-        btnAddProduct4.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
-        btnAddProduct4.setLabel("Add Product");
+        btnAddBrand.setBackground(new java.awt.Color(153, 153, 255));
+        btnAddBrand.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnAddBrand.setLabel("Add Brand");
 
         btnAddProduct5.setBackground(new java.awt.Color(153, 153, 255));
         btnAddProduct5.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
-        btnAddProduct5.setLabel("Add Product");
+        btnAddProduct5.setLabel("View Categories");
 
         jPanel1.setBackground(new java.awt.Color(0, 102, 204));
         jPanel1.setForeground(new java.awt.Color(255, 255, 255));
@@ -86,41 +91,89 @@ public class BrandAdder extends javax.swing.JFrame {
                 .addGap(28, 28, 28))
         );
 
+        btnViewCustomers.setBackground(new java.awt.Color(153, 153, 255));
+        btnViewCustomers.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnViewCustomers.setLabel("View Customers");
+
+        btnDailySales.setBackground(new java.awt.Color(153, 153, 255));
+        btnDailySales.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnDailySales.setLabel("Daily Sales");
+
+        btnTodaySales.setBackground(new java.awt.Color(153, 153, 255));
+        btnTodaySales.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnTodaySales.setLabel("Today Sales");
+
+        btnLogOut.setBackground(new java.awt.Color(153, 153, 255));
+        btnLogOut.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnLogOut.setLabel("Log Out");
+
+        btnViewOrders.setBackground(new java.awt.Color(153, 153, 255));
+        btnViewOrders.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        btnViewOrders.setLabel("View Orders");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnAddProduct3, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnAddProduct2, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnAddProduct4, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnAddProduct1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnAddProduct5, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnAddProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(184, 184, 184))
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(169, 169, 169)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnDailySales, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnTodaySales, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnViewCustomers, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnViewOrders, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnAddBrand, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnViewBrands, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnAddCategory, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnAddProduct5, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnAddProduct3, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnViewProducts, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnLogOut, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(80, 80, 80)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 87, Short.MAX_VALUE)
-                .addComponent(btnAddProduct3, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAddProduct3, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnViewProducts, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnAddProduct2, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(19, 19, 19)
-                .addComponent(btnAddProduct4, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAddCategory, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAddProduct5, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnAddProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(16, 16, 16)
-                .addComponent(btnAddProduct1, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAddBrand, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnViewBrands, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnAddProduct5, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(24, 24, 24))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnViewCustomers, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnViewOrders, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnDailySales, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnTodaySales, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnLogOut, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(108, Short.MAX_VALUE))
         );
 
         pack();
@@ -162,12 +215,17 @@ public class BrandAdder extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private java.awt.Button btnAddProduct;
-    private java.awt.Button btnAddProduct1;
-    private java.awt.Button btnAddProduct2;
+    private java.awt.Button btnAddBrand;
+    private java.awt.Button btnAddCategory;
     private java.awt.Button btnAddProduct3;
-    private java.awt.Button btnAddProduct4;
     private java.awt.Button btnAddProduct5;
+    private java.awt.Button btnDailySales;
+    private java.awt.Button btnLogOut;
+    private java.awt.Button btnTodaySales;
+    private java.awt.Button btnViewBrands;
+    private java.awt.Button btnViewCustomers;
+    private java.awt.Button btnViewOrders;
+    private java.awt.Button btnViewProducts;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
